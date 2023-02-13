373de3ebf8ba7159dcf504f5779f36f475280a68
Arpit Batra <arpitbatra123@gmail.com>
2023-02-13 14:49:45 +0530

    Add tests for retry handler

--- functions/common/twilio-wrappers/retry-handler.private.js
+++ functions/common/twilio-wrappers/retry-handler.private.js
@@ -1,11 +1,8 @@
-const { random, isNumber, isString } = require("lodash");
-
-snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+const { random, isNumber } = require("lodash");
 
 /**
  * @param {object} error the error from the calling function
  * @param {object} parameters the parameters to call the callback with
- * @param {string} parameters.scriptName the name of the top level lambda function 
  * @param {number} parameters.attempts the number of retry attempts performed
  * @param {function} callback the callback function to retry
  * @returns {any}
@@ -26,27 +23,28 @@ snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
  */
 exports.retryHandler = async (error, parameters, callback) => {
 
+  const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+
   if(!isNumber(parameters.attempts))
     throw "Invalid parameters object passed. Parameters must contain the number of attempts";
-  if(!isString(parameters.scriptName))
-    throw "Invalid parameters object passed. Parameters must contain scriptName of calling function";
 
   const { 
     TWILIO_SERVICE_MAX_BACKOFF, 
     TWILIO_SERVICE_MIN_BACKOFF, 
     TWILIO_SERVICE_RETRY_LIMIT } = process.env;
-  const { attempts, scriptName } = parameters;
+  const { attempts, context } = parameters;
   const { response, message: errorMessage } = error;
   const status = response? response.status : 500;
   const logWarning = attempts == 1 ? `${parameters.attempts} retry attempt` : `${parameters.attempts} retry attempts`;
   const message = errorMessage? errorMessage : error;
 
   if (
-    (status == 412 || status == 429 || status == 503)
+    (status == 412 || status == 429 || status == 503) 
     && isNumber(attempts)
     && attempts < TWILIO_SERVICE_RETRY_LIMIT) 
     {
-      console.warn(`retrying ${scriptName}.${callback.name}() after ${logWarning}, status code: ${status}`);
+      console.warn(`retrying ${context.PATH}.${callback.name}() after ${logWarning}, status code: ${status}`);
       if(status === 429 || status === 503)
         await snooze(
           random(
@@ -59,7 +57,7 @@ exports.retryHandler = async (error, parameters, callback) => {
       const updatedParameters = {...parameters, attempts: updatedAttempts};
       return callback(updatedParameters);
   } else {
-    console.error(`retrying ${scriptName}.${callback.name}() failed after ${logWarning}, status code: ${status}, message: ${message}`);
+    console.error(`retrying ${context.PATH}.${callback.name}() failed after ${logWarning}, status code: ${status}, message: ${message}`);
     return { success: false, message, status };
   }
 }
