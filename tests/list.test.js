e6f8197ff92904d8b0df00262fbcb16c453a8e20
Arpit Batra <arpitbatra123@gmail.com>
2023-02-20 15:52:48 +0530

    More failure test cases for list

--- tests/list.test.js
+++ tests/list.test.js
@@ -142,4 +142,133 @@ describe('List Function', () => {
     expect(callback.mock.calls[0][1]._statusCode).toEqual(expected._statusCode);
     expect(callback.mock.calls[0][1]._body.version).toEqual('ConfigSID');
   });
+
+  test('error, no data asset in latest build', async () => {
+    mockFunctionValidatorObject.mockImplementation((fn) => {
+      return fn;
+    });
+    jest.mock('../functions/common/twilio-wrappers/serverless.private', () => ({
+      __esModule: true,
+      fetchLatestBuild: () => {
+        return {
+          success: true,
+          status: 200,
+          latestBuild: {
+            assetVersions: [
+              {
+                path: '/configs.json',
+                sid: 'ConfigSID',
+              },
+            ],
+          },
+        };
+      },
+      fetchLatestDeployment: () => {
+        return {
+          success: true,
+          status: 200,
+          latestDeployment: {
+            path: '/config.json',
+            buildSid: 'ConfigSID',
+          },
+        };
+      },
+    }));
+    const List = require('../functions/admin/list');
+    const event = {};
+    const callback = jest.fn();
+
+    await List.handler(context, event, callback);
+    expect(callback.mock.calls[0][0]).toEqual('Missing asset in latest build');
+  });
+
+  test('latest deployment not successful', async () => {
+    mockFunctionValidatorObject.mockImplementation((fn) => {
+      return fn;
+    });
+    jest.mock('../functions/common/twilio-wrappers/serverless.private', () => ({
+      __esModule: true,
+      fetchLatestBuild: () => {
+        return {
+          success: true,
+          status: 200,
+          latestBuild: {
+            assetVersions: [
+              {
+                path: '/config.json',
+                sid: 'ConfigSID',
+              },
+            ],
+          },
+        };
+      },
+      fetchLatestDeployment: () => {
+        return {
+          success: false,
+          status: 404,
+          message: 'Could not find latest deployment',
+        };
+      },
+    }));
+    const List = require('../functions/admin/list');
+    const event = {};
+    const callback = jest.fn();
+
+    const response = new Twilio.Response();
+    response.appendHeader('Access-Control-Allow-Origin', '*');
+    response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS GET');
+    response.appendHeader('Content-Type', 'application/json');
+    response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
+    response.setStatusCode(404);
+    const expected = response;
+
+    await List.handler(context, event, callback);
+    expect(callback.mock.calls[0][1]._statusCode).toEqual(expected._statusCode);
+    expect(callback.mock.calls[0][1]._body.message).toEqual('Could not find latest deployment');
+  });
+
+  test('schedule utils throws error', async () => {
+    mockFunctionValidatorObject.mockImplementation((fn) => {
+      return fn;
+    });
+    jest.mock('../functions/common/twilio-wrappers/serverless.private', () => ({
+      __esModule: true,
+      fetchLatestBuild: () => {
+        return {
+          success: true,
+          status: 200,
+          latestBuild: {
+            assetVersions: [
+              {
+                path: '/config.json',
+                sid: 'ConfigSID',
+              },
+            ],
+          },
+        };
+      },
+      fetchLatestDeployment: () => {
+        return {
+          success: true,
+          status: 200,
+          latestDeployment: {
+            path: '/config.json',
+            buildSid: 'ConfigSID',
+          },
+        };
+      },
+    }));
+    jest.mock('../functions/common/helpers/schedule-utils.private', () => ({
+      __esModule: true,
+      evaluateSchedule: () => {
+        throw new Error('Eval Schedule Aborted');
+      },
+    }));
+    const List = require('../functions/admin/list');
+    const event = {};
+    const callback = jest.fn();
+
+    await List.handler(context, event, callback);
+    expect(callback.mock.calls[0][0].toString()).toEqual('Error: Eval Schedule Aborted');
+  });
 });
